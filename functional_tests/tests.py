0131e17e400cfae77929ef6a10a39fbdcff79f72
rajjix <rajjix.dev.1992@hotmail.com>
2018-12-02 18:52:35 +0200

    chapter 06

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,6 +1,10 @@
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+
+MAX_WAIT = 10
+
 from django.test import LiveServerTestCase
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
 
 class NewVisitorTest(LiveServerTestCase):
@@ -12,13 +16,21 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_list_table(self, row_text):
+    def wait_for_row_list_table(self, row_text):
         """
         Check if submitted data has been added to the content
         """
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
@@ -32,10 +44,9 @@ class NewVisitorTest(LiveServerTestCase):
                 ) # Check input box placeholder
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
+        self.wait_for_row_list_table('1: Buy peacock feathers')
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
-        self.check_for_row_list_table('1: Buy peacock feathers')
-        self.check_for_row_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_list_table('1: Buy peacock feathers')
