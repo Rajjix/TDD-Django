4e3d3cbc5128ac07ad6386255a1b39fe8a8af95a
rajjix <rajjix.dev.1992@hotmail.com>
2018-12-02 18:33:38 +0200

    chapter 05 0.1

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     
     def setUp(self):
         print('start')
@@ -21,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         self.assertIn('To-Do', self.browser.title) # Check title
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text) # Check header
@@ -39,7 +39,3 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(3)
         self.check_for_row_list_table('1: Buy peacock feathers')
         self.check_for_row_list_table('2: Use peacock feathers to make a fly')
-
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
