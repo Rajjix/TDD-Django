374e2a6ed422ccfb0d656f0909f821d27aee3e15
rajjix <rajjix.dev.1992@hotmail.com>
2018-12-03 04:35:10 +0200

    Chapter 09

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,12 +1,10 @@
 """
 superlists URL Configuration
 """
-from django.contrib import admin
 from django.urls import path, include
 from lists.views import home_page
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', home_page, name='home'),
     path('lists/', include('lists.urls')),
 ]
